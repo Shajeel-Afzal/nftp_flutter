b1aada1d2608ad6cebee2c4640089e617315283d
Shajeel Afzal <shajeel.afzal@gmail.com>
2021-01-11 15:00:27 +0500

    News API Flutter Application Completd!

--- news_api_flutter_project/lib/TopHeadingsScreen.dart
+++ news_api_flutter_project/lib/TopHeadingsScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_api_flutter_project/NewsAPI.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class TopHeadingScreen extends StatelessWidget {
   @override
@@ -10,47 +11,64 @@ class TopHeadingScreen extends StatelessWidget {
       body: FutureBuilder(
         future: NewsAPI().getTopHeadings(),
         builder: (BuildContext context, AsyncSnapshot<http.Response> snapshot) {
-          switch(snapshot.connectionState){
-
+          switch (snapshot.connectionState) {
             case ConnectionState.active:
             case ConnectionState.none:
             case ConnectionState.waiting:
               return Center(
                   child: Column(
-                    mainAxisSize: MainAxisSize.min,
+                mainAxisSize: MainAxisSize.min,
                 children: [
                   Text("Loading Top Headings"),
-                  SizedBox(height: 20,),
+                  SizedBox(
+                    height: 20,
+                  ),
                   CircularProgressIndicator(),
                 ],
               ));
               break;
 
             case ConnectionState.done:
-              return ListView.builder(
-                itemCount: 5,
-                itemBuilder: (BuildContext context, int index){
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      child: Column(
-                        children: [
-                          ListTile(title: Text("BBC News"), trailing: Text("2021-01-09T07:29:00Z"),),
-                          Image.network("https://ichef.bbci.co.uk/news/1024/branded_news/32E6/production/_116403031_065099522-2.jpg"),
-                          ListTile(title: Text("Trump riots: Democrats plan to introduce article of impeachment - BBC News"))
-                        ],
+              if (snapshot.hasData) {
+                var response = snapshot.data;
+                var responseBody = response.body;
+                Map<String, dynamic> responseJSON = jsonDecode(responseBody);
+                var statusCode = responseJSON["status"];
+                var totalResults = responseJSON["totalResults"];
+                var articlesArray = responseJSON["articles"];
+
+                print(responseBody);
+
+                return ListView.builder(
+                  itemCount: totalResults,
+                  itemBuilder: (BuildContext context, int index) {
+                    Map<String, dynamic> newsObject = articlesArray[index];
+
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Text(newsObject["source"]["name"]),
+                              trailing: Text("2021-01-09T07:29:00Z"),
+                            ),
+                            Image.network(newsObject["urlToImage"] == null
+                                ? ""
+                                : newsObject["urlToImage"]),
+                            ListTile(title: Text(newsObject["title"]))
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
-              );
+                    );
+                  },
+                );
+              }
+
               break;
           }
         },
-
       ),
     );
   }
 }
-
-
