c738e7c59c3e25c34f07e69b27319a1a37c05b5b
Shajeel Afzal <shajeel.afzal@gmail.com>
2021-01-21 15:31:41 +0500

    created new project and configured firebase for Android

--- firebase_authentication/lib/timeline_screen.dart
+++ firebase_authentication/lib/timeline_screen.dart
@@ -34,7 +34,7 @@ class TimeLineScreen extends StatelessWidget {
             context,
             MaterialPageRoute(
               builder: (BuildContext context) {
-                return PostScreen();
+                return LoginScreen();
               },
             ),
           );
---
+++ firebase_flutter_social/android/app/src/main/kotlin/com/example/firebase_flutter_social/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_flutter_social
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
